b521064320dfbdd16d1997d88bba2de6fa0cd4cd
Kasun <kasun.w@duosoftware.com>
2017-09-04 15:33:35 +0530

    Access control added

--- bulkupload/bulkupload.module.js
+++ bulkupload/bulkupload.module.js
@@ -8,60 +8,71 @@
 
 (function ()
 {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('app.bulkupload', [])
-    .config(config)
-    .filter('parseDate',parseDateFilter);
+	angular
+		.module('app.bulkupload', [])
+		.config(config)
+		.filter('parseDate',parseDateFilter);
 
-  /** @ngInject */
-  function config($stateProvider, $translatePartialLoaderProvider, msApiProvider, msNavigationServiceProvider, mesentitlementProvider)
-  {
+	/** @ngInject */
+	function config($stateProvider, $translatePartialLoaderProvider, msApiProvider, msNavigationServiceProvider, mesentitlementProvider)
+	{
 
-    mesentitlementProvider.setStateCheck("bulkupload");
+		// State
+		$stateProvider
+			.state('app.bulkupload', {
+				url    : '/bulkupload',
+				views  : {
+					'bulkupload@app': {
+						templateUrl: 'app/main/bulkupload/bulkupload.html',
+						controller : 'BulkuploadController as vm'
+					}
+				},
+				resolve: {
+					security: ['$q','mesentitlement','$timeout','$rootScope','$state','$location', function($q,mesentitlement,$timeout,$rootScope,$state, $location){
+						return $q(function(resolve, reject) {
 
-    // State
-    $stateProvider
-      .state('app.bulkupload', {
-        url    : '/bulkupload',
-        views  : {
-          'bulkupload@app': {
-            templateUrl: 'app/main/bulkupload/bulkupload.html',
-            controller : 'BulkuploadController as vm'
-          }
-        },
-        resolve: {
-          security: ['$q','mesentitlement', function($q,mesentitlement){
-            // var entitledStatesReturn = mesentitlement.stateDepResolver('bulkupload');
-			//
-            // if(entitledStatesReturn !== true){
-            //   return $q.reject("unauthorized");
-            // };
-          }]
-        },
-        bodyClass: 'bulkupload'
-      });
+							$timeout(function() {
+								if ($rootScope.isBaseSet2) {
+									resolve(function () {
+										var entitledStatesReturn = mesentitlement.stateDepResolver('bulkupload');
 
-    //Api
-    msApiProvider.register('cc_invoice.invoices', ['app/data/cc_invoice/invoices.json']);
+										mesentitlementProvider.setStateCheck("bulkupload");
 
-    // Navigation
+										if(entitledStatesReturn !== true){
+											return $q.reject("unauthorized");
+										}
+									});
+								} else {
+									return $location.path('/guide');
+								}
+							});
+						});
+					}]
+				},
+				bodyClass: 'bulkupload'
+			});
 
-    msNavigationServiceProvider.saveItem('bulkupload', {
-      title    : 'bulkupload',
-      icon     : 'icon-leaf',
-      state    : 'app.bulkupload',
-      /*stateParams: {
-       'param1': 'page'
-       },*/
-      weight   : 11
-    });
-  }
+		//Api
+		//msApiProvider.register('cc_invoice.invoices', ['app/data/cc_invoice/invoices.json']);
 
-  function parseDateFilter(){
-    return function(input){
-      return new Date(input);
-    };
-  }
+		// Navigation
+
+		msNavigationServiceProvider.saveItem('bulkupload', {
+			title    : 'bulkupload',
+			icon     : 'icon-leaf',
+			state    : 'app.bulkupload',
+			/*stateParams: {
+			 'param1': 'page'
+			 },*/
+			weight   : 11
+		});
+	}
+
+	function parseDateFilter(){
+		return function(input){
+			return new Date(input);
+		};
+	}
 })();
